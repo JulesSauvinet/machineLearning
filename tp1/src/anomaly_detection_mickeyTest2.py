fe9e03c393467d28777babcfa1f945be9a929941
Marine RUIZ <Marine RUIZ>
2017-02-01 23:59:46 +0100

    Module de detection a adapter

--- tp1/src/anomaly_detection_mickeyTest2.py
+++ tp1/src/anomaly_detection_mickeyTest2.py
@@ -16,6 +16,7 @@ from prediction.lof import outliers
 from text_mining import textMining
 
 
+# -------------------------------------------------------------------------------------------------------------------------#
 def showRawDatas(df):
     x = df.values[:, 0]
     y = df.values[:, 1]
@@ -25,11 +26,6 @@ def showRawDatas(df):
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
-
-df3=pd.read_csv('../data/mouse-synthetic-data.txt', sep=' ')
-showRawDatas(df3)
-
-
 # -------------------------------------------------------------------------------------------------------------------------#
 
 # -------------------------------------------------------------------------------------------------------------------------#
@@ -53,44 +49,43 @@ def detectAnomaly(X,plot=True):
     ground_truth = np.ones(n_samples, dtype=int)
     ground_truth[-n_outliers:] = -1
 
+    # X is the our data and it contains 20 outliers to 500 data rows
+    
     # Fit the problem with varying cluster separation
     for i, offset in enumerate(clusters_separation):
         np.random.seed(42)
-        # Data generation
-        X1 = 0.3 * np.random.randn(n_inliers // 2, 2) - offset
-        X2 = 0.3 * np.random.randn(n_inliers // 2, 2) + offset
-        X = np.r_[X1, X2]
-        # Add outliers
-        X = np.r_[X, np.random.uniform(low=-6, high=6, size=(n_outliers, 2))]
 
         # Fit the model
         plt.figure(figsize=(10.8, 3.6))
+        
         for i, (clf_name, clf) in enumerate(classifiers.items()):
+            
             # fit the data and tag outliers
             clf.fit(X)
             scores_pred = clf.decision_function(X)
-            threshold = stats.scoreatpercentile(scores_pred,
-                                                100 * outliers_fraction)
+            threshold = stats.scoreatpercentile(scores_pred, 100 * outliers_fraction)
             y_pred = clf.predict(X)
             n_errors = (y_pred != ground_truth).sum()
+            
             # plot the levels lines and the points
             Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
             Z = Z.reshape(xx.shape)
+            
             subplot = plt.subplot(1, 3, i + 1)
-            subplot.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),
-                             cmap=plt.cm.Blues_r)
-            a = subplot.contour(xx, yy, Z, levels=[threshold],
-                                linewidths=2, colors='red')
-            subplot.contourf(xx, yy, Z, levels=[threshold, Z.max()],
-                             colors='orange')
+            subplot.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),cmap=plt.cm.Blues_r)
+            
+            a = subplot.contour(xx, yy, Z, levels=[threshold],linewidths=2, colors='red')
+            
+            subplot.contourf(xx, yy, Z, levels=[threshold, Z.max()],colors='orange')
+            
             b = subplot.scatter(X[:-n_outliers, 0], X[:-n_outliers, 1], c='white')
             c = subplot.scatter(X[-n_outliers:, 0], X[-n_outliers:, 1], c='black')
+            
             subplot.axis('tight')
-            subplot.legend(
-                [a.collections[0], b, c],
-                ['learned decision function', 'true inliers', 'true outliers'],
-                prop=matplotlib.font_manager.FontProperties(size=11),
-                loc='lower right')
+            subplot.legend( [a.collections[0], b, c],
+                            ['learned decision function', 'true inliers', 'true outliers'],
+                            prop=matplotlib.font_manager.FontProperties(size=11),
+                            loc='lower right')
             subplot.set_title("%d. %s (errors: %d)" % (i + 1, clf_name, n_errors))
             subplot.set_xlim((-7, 7))
             subplot.set_ylim((-7, 7))
@@ -102,6 +97,7 @@ def detectAnomaly(X,plot=True):
 # Test sur la base de données Mouse
 
 df3=pd.read_csv('../data/mouse-synthetic-data.txt', sep=' ')
+showRawDatas(df3)
 X = df3.values[:, [0,1]]
 print 'Detection des anomalies'
 detectAnomaly(X)
